621f95f8a49682bfe2bad3edf17a3057dd7ac82d
zhexuany <yangzhexuan@pingcap.com>
2017-07-29 17:15:25 +0800

    fix index type

--- src/main/java/com/pingcap/tikv/meta/IndexType.java
+++ src/main/java/com/pingcap/tikv/meta/IndexType.java
@@ -18,8 +18,9 @@ package com.pingcap.tikv.meta;
 import com.pingcap.tikv.exception.TiClientInternalException;
 
 public enum IndexType {
-  IndexTypeBtree(0),
-  IndexTypeHash(1);
+  IndexTypeInvalid(0),
+  IndexTypeBtree(1),
+  IndexTypeHash(2);
 
   private final int type;
 
