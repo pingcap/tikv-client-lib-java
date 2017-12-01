a7f878bd8653731f6fd100c97c0e460f6099df8a
Soup <maxiaoyu@pingcap.com>
2017-12-01 17:56:49 +0800

    Add versioning record (#172)

--- scripts/version.sh
+++ scripts/version.sh
@@ -14,18 +14,7 @@
 #   limitations under the License.
 #
 
-if hash google-java-format 2>/dev/null; then 
-	files_need_format=()
-	files=$(find ../src -name "*.java")
-	for file in "${files[@]}";do
-		echo "formatting..."
-		#google-java-format -r ("${file}")
-		google-java-format -r ${file}
-	done
-else
-	echo "google-java-format is not installed, please install it first"
-	echo "If you are using macos, type 'brew install google-java-format'"
-	echo "Otherwise, go to google-java-format github page:
-	https://github.com/google/google-java-format.git
-   	and download it."
-fi
+SHA1=`git describe --tags`
+echo '
+package com.pingcap.tikv;
+public final class TiVersion { public final static String CommitVersion = "'${SHA1}'"; }' > src/main/java/com/pingcap/tikv/TiVersion.java
